d657dde72ccf2ba55b8463522bf719f058ffab3e
PureDiors <xiongzhitong@gmail.com>
2016-01-06 10:39:15 +0800

    zeiler.lua

--- cnn_lstm.lua
+++ cnn_lstm.lua
@@ -5,11 +5,13 @@ require 'cudnn'
 require 'cunn'
 require 'base'
 require 'sys'
+require 'paths'
+require 'paths'
+require 'nn'
+require 'inn'
 
 
 --nngraph.setDebug(true)
-
-
 --------------------------------------------
 local function debug(vname, vvalue )
   print('-----------'.. vname..'------------')
@@ -22,7 +24,7 @@ local params = {batch_size=1,
                 seq_length=5,
                 layers=1,
                 decay=2,
-                rnn_size=2048,
+                rnn_size=1000,
                 dropout=0,
                 init_weight=0.1,
                 lr=1,
@@ -70,58 +72,25 @@ local function lstm(x, prev_c, prev_h)
 end
 
 
--------------Create a CNN using alex net model and cuDNN v3.0-----------------
-local function alexmodel()
-  local SpatialConvolution = cudnn.SpatialConvolution--lib[1]
-  local SpatialMaxPooling = cudnn.SpatialMaxPooling--lib[2]
-  -- from https://code.google.com/p/cuda-convnet2/source/browse/layers/layers-imagenet-1gpu.cfg
-  -- this is AlexNet that was presented in the One Weird Trick paper. http://arxiv.org/abs/1404.5997
-  local features = nn.Sequential()
-  features:add(SpatialConvolution(3,64,11,11,2,2,1,1))       --  80 * 80 -> 36 * 36
-  features:add(cudnn.ReLU(true))
-  features:add(SpatialMaxPooling(2,2,1,1))                   --  36 * 36 -> 35 * 35 
-  features:add(SpatialConvolution(64,192,5,5,1,1,2,2))       --  35 * 35
-  features:add(cudnn.ReLU(true))
-  features:add(SpatialMaxPooling(3,3,2,2))                   --  (35-3)/2 + 1 = 17 | 17 * 17
-  features:add(SpatialConvolution(192,384,3,3,1,1,1,1))      --  17 * 17
-  features:add(cudnn.ReLU(true))
-  features:add(SpatialConvolution(384,256,3,3,1,1,1,1))      --  17 * 17
-  features:add(cudnn.ReLU(true))
-  features:add(SpatialConvolution(256,256,3,3,1,1,1,1))      --  17 * 17
-  features:add(cudnn.ReLU(true))
-  features:add(SpatialMaxPooling(3,3,2,2))                   -- 17 -> 8
-  features:add(nn.View(256*8*8))
-  features:add(nn.Dropout(0.5))
-  features:add(nn.Linear(256*8*8, 4096))
-  features:add(nn.Threshold(0, 1e-6))
-  features:add(nn.Dropout(0.5))
-  features:add(nn.Linear(4096, 2048))
-  features:add(nn.Threshold(0, 1e-6))
-  
 
-  return {
-    model = features,
-    regime = {
-      epoch        = {1,    19,   30,   44,   53  },
-      learningRate = {1e-2, 5e-3, 1e-3, 5e-4, 1e-4},
-      weightDecay  = {5e-4, 5e-4, 0,    0,    0   }
-    }
-  }
-end
+-------------------CNN model------------------------
+local createModel = paths.dofile('zeiler.lua')
+local backend = 'cudnn'
+local cnnmodel = createModel(backend)
 
 
 ---------------Create the whole network by nngraph--------------
 local function create_network()
+
   local x                = nn.Identity()()
   
-  local alexnet = alexmodel()
-  local alex = alexnet.model
-  local cnn_output = alex(x)
+  local cnn_model = cnnmodel
+  local cnn_output = cnn_model(x)
   local lstm_input = nn.Identity()(cnn_output)
-  local reshaped = nn.Reshape(1,2048)(lstm_input)
+  local reshaped = nn.Reshape(1,1000)(lstm_input)
   local feat_input = {[0] = reshaped}  --input size 1 * 2048
 
-  local y                = nn.Identity()()
+  --local y                = nn.Identity()()
   local prev_s           = nn.Identity()()
  
   local next_s           = {}
@@ -137,11 +106,11 @@ local function create_network()
   local h2y              = nn.Linear(params.rnn_size, params.n_output)
   local dropped          = nn.Dropout(params.dropout)(feat_input[params.layers])
   local pred             = nn.LogSoftMax()(h2y(dropped))
-  local err              = nn.MSECriterion()({pred, y})
+  --local err              = nn.MSECriterion()({pred, y})
   --For debug 
-  nngraph.annotateNodes()
-  local module           = nn.gModule({x, y, prev_s},
-                                      {err, nn.Identity()(next_s)})
+  --nngraph.annotateNodes()
+  local module           = nn.gModule({x,  prev_s},
+                                      {pred, nn.Identity()(next_s)})
 
 
   module:getParameters():uniform(-params.init_weight, params.init_weight)
@@ -154,7 +123,7 @@ end
 -------------------------Setup the whole network---------------------------
 
 local function setup()
-  print("Creating a  CNN-LSTM network.")
+  print("Creating a CNN-LSTM network...")
   local core_network = create_network()
   paramx, paramdx = core_network:getParameters()
 
@@ -164,8 +133,11 @@ local function setup()
   model.s = {}
   model.ds = {}
   model.start_s = {}
+  model.preds = {}
+  criterion = nn.MSECriterion()
   for j = 0, params.seq_length do
     model.s[j] = {}
+    model.preds[j] = transfer_data(torch.zeros(params.n_output))
     --prev_s contains prev_h and prev_c
     for d = 1, 2 * params.layers do
       model.s[j][d] = transfer_data(torch.zeros(params.batch_size, params.rnn_size))
@@ -198,8 +170,6 @@ local function reset_ds()
   end
 end
 
-
-
 -------------------------------feedforward----------------------
 
 local function fp(state)
@@ -211,11 +181,12 @@ local function fp(state)
     local x = state.data[state.pos]
     local y = state.label[state.pos]
     local s = model.s[i - 1]
-    model.err[i], model.s[i] = unpack(model.rnns[i]:forward({x, y, s}))
+    model.preds[i], model.s[i] = unpack(model.rnns[i]:forward({x, s}))
+    model.err[i] = criterion:forward(model.preds[i],y)
     state.pos = state.pos + 1
   end
   g_replace_table(model.start_s, model.s[params.seq_length])
-  return model.err:mean()
+  return model.preds,model.err:mean()
 end
 
 ------------------------------backforward------------------------
@@ -227,9 +198,10 @@ local function bp(state)
     local x = state.data[state.pos]
     local y = state.label[state.pos]
     local s = model.s[i - 1]
-    local derr = transfer_data(torch.ones(1))
-    local tmp = model.rnns[i]:backward({x, y, s},
-                                       {derr, model.ds})[3]
+    --local derr = transfer_data(torch.ones(1))
+    local gradCriterion = transfer_data(criterion:backward(model.preds[i],y))
+    local tmp = model.rnns[i]:backward({x, s},
+                                       {gradCriterion, model.ds})[3]
     g_replace_table(model.ds, tmp)
     cutorch.synchronize()
   end
@@ -246,7 +218,7 @@ end
 
 torch.manualSeed(1234)
 
-input = torch.rand(5,3,80,80):cuda()
+input = torch.rand(5,3,96,96):cuda()
 
 setup()
 
@@ -255,12 +227,13 @@ train_data = {}
 train_data.pos = 1
 train_data.data = input
 train_data.label = label
-data = torch.rand(1,2048):cuda()
-pres = {}
+data = torch.rand(1,1000):cuda()
+preds = {}
 local start = torch.tic()
-local err = fp(train_data)
+local preds,err = fp(train_data)
 local endtime = torch.toc(start)
 print(err)
 print(string.format('Time using:  %.5f seconds',endtime))
-
-
+for i = 1 ,#preds do
+  print(preds[i])
+end
